c5247bb6d045e20319ac6f80438c35d4cff5069e
BullyWiiPlaza <bullywiihacks@gmail.com>
2021-12-31 20:53:28 +0100

    * Upgrade packages references * Refactor code

--- MainForm.cs
+++ MainForm.cs
@@ -6,9 +6,12 @@ using System.IO;
 using System.Net;
 using System.Windows.Forms;
 
-namespace Minecraft_Wii_U_Mod_Injector_Updater {
-    public partial class MainForm : MetroForm {
-        public MainForm() {
+namespace Minecraft_Wii_U_Mod_Injector_Updater
+{
+    public partial class MainForm : MetroForm
+    {
+        public MainForm()
+        {
             InitializeComponent();
         }
 
@@ -16,46 +19,62 @@ namespace Minecraft_Wii_U_Mod_Injector_Updater {
         public string TempPath = $@"{Application.StartupPath}\Minecraft.Wii.U.Mod.Injector.Temp.exe";
         public string TargetPath = $@"{Application.StartupPath}\Minecraft.Wii.U.Mod.Injector.exe";
 
-        private void StartDownloading(object sender, EventArgs e) {
+        private void StartDownloading(object sender, EventArgs e)
+        {
             autoStart.Checked = Properties.Settings.Default.AutoStart;
             
-            var wc = new WebClient();
+            var webClient = new WebClient();
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-            wc.DownloadProgressChanged += DownloadProgress;
-            wc.DownloadFileCompleted += Completed;
-            wc.DownloadFileAsync(new Uri(DownloadUrl), TempPath);
+            webClient.DownloadProgressChanged += DownloadProgress;
+            webClient.DownloadFileCompleted += Completed;
+            webClient.DownloadFileAsync(new Uri(DownloadUrl), TempPath);
         }
 
-        private void DownloadProgress(object sender, DownloadProgressChangedEventArgs e) {
+        private void DownloadProgress(object sender, DownloadProgressChangedEventArgs e)
+        {
             statusLabel.Text = string.Format("Downloading... [ {0}/{1} MB ]", (e.BytesReceived / 1024.0 / 1024.0).ToString("0.00"), (e.TotalBytesToReceive / 1024.0 / 1024.0).ToString("0.00"));
             progressBar.Value = e.ProgressPercentage;
         }
 
-        private void Completed(object sender, AsyncCompletedEventArgs e) {
-            if (e.Cancelled) {
+        private void Completed(object sender, AsyncCompletedEventArgs e)
+        {
+            if (e.Cancelled)
+            {
                 Console.WriteLine("Mod Injector download aborted");
-            } else {
-                if (File.Exists(TargetPath)) File.Delete(TargetPath);
+            } else
+            {
+                if (File.Exists(TargetPath))
+                {
+                    File.Delete(TargetPath);
+                }
+
                 File.Move(TempPath, TargetPath);
                 Console.WriteLine("Mod Injector download complete");
             }
             statusLabel.Text = "Finished!";
-            if (autoStart.Checked) {
+            if (autoStart.Checked)
+            {
                 Process.Start(TargetPath);
                 Environment.Exit(0);
-            } else {
+            } else
+            {
                 startButton.Enabled = true;
             }
         }
 
-        private void ChangeAutoStart(object sender, EventArgs e) {
+        private void ChangeAutoStart(object sender, EventArgs e)
+        {
             if (autoStart.Checked)
+            {
                 MessageBox.Show("The Mod Injector will now automatically start the next time the updater is run.", "AutoStart", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+
             Properties.Settings.Default.AutoStart = autoStart.Checked;
             Properties.Settings.Default.Save();
         }
 
-        private void Launch(object sender, EventArgs e) {
+        private void Launch(object sender, EventArgs e)
+        {
             Process.Start(TargetPath);
             Environment.Exit(0);
         }
--- Program.cs
+++ Program.cs
@@ -1,18 +1,25 @@
 ﻿using System;
 using System.Windows.Forms;
 
-namespace Minecraft_Wii_U_Mod_Injector_Updater {
-    static class Program {
+namespace Minecraft_Wii_U_Mod_Injector_Updater
+{
+    static class Program
+    {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main(string[] args)
+        static void Main(string[] arguments)
         {
-            if (args.Length == 0)
-                MainForm.DownloadUrl = "https://github.com/Kashiiera/Minecraft-Wii-U-Mod-Injector/releases/latest/download/Minecraft.Wii.U.Mod.Injector.exe";
+            var repositoryBaseUrl = "https://github.com/Kashiiera/Minecraft-Wii-U-Mod-Injector";
+            if (arguments.Length == 0)
+            {
+                MainForm.DownloadUrl = repositoryBaseUrl + "/releases/latest/download/Minecraft.Wii.U.Mod.Injector.exe";
+            }
             else
-                MainForm.DownloadUrl = "https://github.com/Kashiiera/Minecraft-Wii-U-Mod-Injector/releases/" + args[0] + "/download/Minecraft.Wii.U.Mod.Injector.exe";
+            {
+                MainForm.DownloadUrl = repositoryBaseUrl + "/releases/" + arguments[0] + "/download/Minecraft.Wii.U.Mod.Injector.exe";
+            }
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
